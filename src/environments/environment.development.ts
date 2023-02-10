9d9663774ad83ded7bf365ac8e320898bfbd02be
Offram <sachinlagamuwa@gmail.com>
2023-02-10 18:28:53 +0530

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    toastError: {
+        nzPlacement: 'bottomRight',
+        nzDuration: 6000,
+        nzPauseOnHover: true,
+    },
+    toastSuccess: {
+        nzPlacement: 'bottomRight',
+        nzDuration: 6000,
+        nzPauseOnHover: true,
+    },
+};
